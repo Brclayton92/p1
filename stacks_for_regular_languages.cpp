7bf63aa369ca2c503c64725761201999c8337115
Brock <Brock.clayton92@gmail.com>
2018-06-15 13:30:27 -0400

    0.2
    -Added some comments to L1 method

--- stacks_for_regular_languages.cpp
+++ stacks_for_regular_languages.cpp
@@ -9,6 +9,8 @@ bool stacks_for_regular_languages::L1(char *inputString) {
     stack_p1<char> newStack;
     char first = *inputString;
 
+    // iterates through string checking for non 'A' and 'B characters and adding the first character of the string to the
+    // stack every time it is encountered.
     for(char* it = inputString; *it; ++it) {
         if (*it == 'A' || *it =='B'){
             if (*it == first)
@@ -20,18 +22,22 @@ bool stacks_for_regular_languages::L1(char *inputString) {
         }
     }
 
+    // iterates through the string again, popping off the stack each time a character that is not
+    // the first character of the string is encountered
     for(char* it = inputString; *it; ++it) {
-        if (*it != first){
-            if (newStack.isEmpty() != true) {
+        if (*it != first){ // first if statement is entered every time current character is not "first character"
+            if (newStack.isEmpty() != true) { // pops from that for each "non first" character encountered
                 newStack.pop();
             }
 
-            else {
+            else { // if a "non first" character is encountered, but stack is already empty, returns false.
                 return false;
             }
         }
     }
 
+    // if first if statement is passed, and stack is empty, string fits the language => return true.
+    // otherwise return false
     if (newStack.isEmpty() == true){
         return true;
     }
